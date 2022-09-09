ac070fc228281cf07170ea7166f01a4afb658820
Relieveddd <457425537@qq.com>
2022-09-09 16:55:53 +0800

    <div>     <input type="text" />   </div>   // Debounce (防抖): 每次触发事件时设置延迟调用函数，在此期间取消以前触发的事件. function handleDebounce(funcFollowIn, numDelayIn) {   let timerInner = null;   return (...args) => {     if (timerInner) clearTimeout(timerInner);     timerInner = setTimeout(() => {       funcFollowIn.apply(this, args);     }, numDelayIn);   }; } // Throttle (节流): 事件占用一个时间段, 在此时间段内不会再接受事件触发. function handleThrottle(funcFollowIn, numDelayIn) {   let timerInner = null;   return (...args) => {     if (!timerInner) {       timerInner = setTimeout(() => {         funcFollowIn.apply(this, args);         timerInner = null;       }, numDelayIn);     }   }; }

--- src/views/DebounceAndThrottle.vue
+++ src/views/DebounceAndThrottle.vue
@@ -1,6 +1,31 @@
 <template>
+  <div>
+    <input type="text" />
+  </div>
 </template>
 <script setup>
+// Debounce (防抖): 每次触发事件时设置延迟调用函数，在此期间取消以前触发的事件.
+function handleDebounce(funcFollowIn, numDelayIn) {
+  let timerInner = null;
+  return (...args) => {
+    if (timerInner) clearTimeout(timerInner);
+    timerInner = setTimeout(() => {
+      funcFollowIn.apply(this, args);
+    }, numDelayIn);
+  };
+}
+// Throttle (节流): 事件占用一个时间段, 在此时间段内不会再接受事件触发.
+function handleThrottle(funcFollowIn, numDelayIn) {
+  let timerInner = null;
+  return (...args) => {
+    if (!timerInner) {
+      timerInner = setTimeout(() => {
+        funcFollowIn.apply(this, args);
+        timerInner = null;
+      }, numDelayIn);
+    }
+  };
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
